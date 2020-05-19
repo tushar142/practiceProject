94ecfd00ffc02d6cf9a8e57348a3f67814692ce5
tushar142 <54470026+tushar142@users.noreply.github.com>
2020-05-19 18:27:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+
+// const slider = document.querySelector('.slide').children;
+
+// const imgs = document.querySelector('.custo_img').children;
+	
+
+//  for (var i =0; i <imgs.length ; i++) {
+//  		imgs[i].addEventListener("click", function () {
+//  			// console.log(this)
+//  			for (var j =0; j<imgs.length; j++) {
+//  				imgs[j].classList.remove("active")
+//  			}
+//  			this.classList.add('active')
+//  			const id = this.getAttribute("data-id")
+//  			console.log(id)
+//  			for (var j =0; j <slider.length; j++) {
+//  				slider[j].classList.remove("active")
+//  			}
+//  			slider[id].classList.add("active")
+//  		})
+//  }
+
+let lines = document.querySelector(".line");
+
+let sideNav = document.querySelector(".header_nav");
+
+let modal = document.querySelector(".modal")
+
+let head = document.querySelector(".header_wraper")
+
+
+lines.addEventListener("click", function(){
+	sideNav.classList.toggle('active')
+	modal.classList.toggle('modalblock')
+})
+
+modal.addEventListener("click", function(){
+	sideNav.classList.remove('active')
+	modal.classList.remove('modalblock')
+})
+
+ window.onscroll = function() {
+      var top = window.scrollY;
+
+      if (top >= 24) {
+        head.classList.add('headbg')
+      }else {
+        head.classList.remove('headbg')
+      }
+    };
+
+
+
+ // jQuery=====================================
+
+
+ $('.owl-carousel').owlCarousel({
+ 	autoplay:true,
+ 	dots:false,
+ 	nav:true,
+    loop:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
